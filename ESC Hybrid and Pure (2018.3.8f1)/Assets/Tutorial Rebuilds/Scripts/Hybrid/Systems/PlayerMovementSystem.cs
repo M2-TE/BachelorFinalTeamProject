21a2d54847c1b2808e2f363c52943b3c592e0c65
Jan <33833587+Model2TypeE@users.noreply.github.com>
2019-03-14 03:49:44 +0100

    Tutorial ECS rebuilt

---
+++ ESC Hybrid and Pure (2018.3.8f1)/Assets/Tutorial Rebuilds/Scripts/Hybrid/Systems/PlayerMovementSystem.cs
@@ -0,0 +1,26 @@
+﻿using Tutorial.Hybrid.Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Tutorial.Hybrid.Systems
+{
+	public class PlayerMovementSystem : ComponentSystem
+	{
+		private struct Group
+		{
+			public Transform Transform;
+			public PlayerInput PlayerInput;
+			public Speed Speed;
+		}
+
+		protected override void OnUpdate()
+		{
+			foreach (var entity in GetEntities<Group>())
+			{
+				Vector3 position = entity.Transform.position;
+				position.x += entity.Speed.Value * Time.deltaTime * entity.PlayerInput.Horizontal;
+				entity.Transform.position = position;
+			}
+		}
+	}
+}
