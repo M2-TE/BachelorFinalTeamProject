d66fc9688f41617cc590618858dffa21779984ed
Jan <33833587+Model2TypeE@users.noreply.github.com>
2019-03-14 16:38:34 +0100

    Testing Playgrounds working

---
+++ ECS Testing Grounds (2018.3.8f1)/Assets/Code/ECS/Components/TestComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct Test : IComponentData
+{
+	public float Value;
+}
+
+public class TestComponent : ComponentDataProxy<Test> { }
