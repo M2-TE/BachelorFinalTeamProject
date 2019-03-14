7780c28d3f39e53683f12cab44a3cfd08bcefac3
Jan <33833587+Model2TypeE@users.noreply.github.com>
2019-03-14 06:02:28 +0100

    Rearranged + small fixes

---
+++ ESC Hybrid and Pure (2018.3.8f1)/Assets/Code/Tutorial Rebuild/Pure/Systems/PlayerInputSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tutorial.Pure.Components;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace Tutorial.Pure.Systems
+{
+	public class PlayerInputSystem : JobComponentSystem
+	{
+		private struct PlayerInputJob : IJobProcessComponentData<PlayerInput>
+		{
+			public float Horizontal;
+
+			public void Execute(ref PlayerInput input)
+			{
+				input.Horizontal = Horizontal;
+			}
+		}
+		protected override JobHandle OnUpdate(JobHandle inputDeps)
+		{
+			var job = new PlayerInputJob
+			{
+				Horizontal = Input.GetAxis("Horizontal")
+			};
+
+			return job.Schedule(this, inputDeps);
+		}
+	}
+}
