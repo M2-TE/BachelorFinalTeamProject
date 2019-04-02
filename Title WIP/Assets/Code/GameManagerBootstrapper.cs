cb7437cca64d01eed692be95e676193ded85de66
Jan <33833587+Model2TypeE@users.noreply.github.com>
2019-04-02 03:58:50 +0200

    removed samples project, started main project with currently missing title

---
+++ Title WIP/Assets/Code/GameManagerBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameManagerBootstrapper : MonoBehaviour
+{
+	[SerializeField] internal Camera mainCam;
+
+	private void OnEnable() => GameManager.Instance.RegisterBootstrapper(this);
+	private void OnDisable() => GameManager.Instance.UnregisterBootstrapper();
+
+	private void Awake()
+	{
+		
+	}
+
+	private void Start()
+	{
+		
+	}
+}
