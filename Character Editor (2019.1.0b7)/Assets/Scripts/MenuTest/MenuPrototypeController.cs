fcaa4ac7f482e01ec2f80e01c9fe67381871a4aa
YDenker <33430355+YDenker@users.noreply.github.com>
2019-04-07 16:41:12 +0200

    Menu Prototype

---
+++ Character Editor (2019.1.0b7)/Assets/Scripts/MenuTest/MenuPrototypeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPrototypeController : MonoBehaviour
+{
+    [SerializeField] private GameObject gameStartButtons;
+    [SerializeField] private GameObject onlineGameButtons;
+
+    private void Start()
+    {
+        gameStartButtons.SetActive(false);
+        onlineGameButtons.SetActive(false);
+    }
+
+    public void SelectStartGame()
+    {
+        onlineGameButtons.SetActive(false);
+        gameStartButtons.SetActive(true);
+    }
+
+    public void SelectOptions()
+    {
+
+    }
+
+    public void SelectBuildingTool()
+    {
+
+    }
+
+    public void SelectOnlineGame()
+    {
+        onlineGameButtons.SetActive(true);
+    }
+
+    public void SelectExitGame()
+    {
+        Application.Quit();
+    }
+}
