2631a5067bf17dccfd7a4239ed035536487fa2f7
YDenker <33430355+YDenker@users.noreply.github.com>
2019-04-11 15:08:29 +0200

    edited Menu Prototype

--- Character Editor (2019.1.0b7)/Assets/Scripts/MenuTest/MenuPrototypeController.cs
+++ Character Editor (2019.1.0b7)/Assets/Scripts/MenuTest/MenuPrototypeController.cs
@@ -6,22 +6,32 @@ public class MenuPrototypeController : MonoBehaviour
 {
     [SerializeField] private GameObject gameStartButtons;
     [SerializeField] private GameObject onlineGameButtons;
+    [SerializeField] private GameObject localGameButtons;
+
+    [SerializeField] private GameObject optionsPanel;
 
     private void Start()
     {
         gameStartButtons.SetActive(false);
         onlineGameButtons.SetActive(false);
+        localGameButtons.SetActive(false);
+        optionsPanel.SetActive(false);
     }
 
     public void SelectStartGame()
     {
+        optionsPanel.SetActive(false);
         onlineGameButtons.SetActive(false);
+        localGameButtons.SetActive(false);
         gameStartButtons.SetActive(true);
     }
 
     public void SelectOptions()
     {
-
+        optionsPanel.SetActive(true);
+        onlineGameButtons.SetActive(false);
+        localGameButtons.SetActive(false);
+        gameStartButtons.SetActive(false);
     }
 
     public void SelectBuildingTool()
@@ -29,9 +39,16 @@ public class MenuPrototypeController : MonoBehaviour
 
     }
 
+    public void SelectLocalGame()
+    {
+        onlineGameButtons.SetActive(false);
+        localGameButtons.SetActive(true);
+    }
+
     public void SelectOnlineGame()
     {
         onlineGameButtons.SetActive(true);
+        localGameButtons.SetActive(false);
     }
 
     public void SelectExitGame()
