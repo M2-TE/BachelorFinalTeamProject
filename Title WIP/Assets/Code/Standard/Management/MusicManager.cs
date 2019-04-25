060ba8851f59104bbb2d3a7da9946908b575f5e0
Jan <33833587+Model2TypeE@users.noreply.github.com>
2019-04-25 22:40:44 +0200

    audio stuff

--- Title WIP/Assets/Code/Standard/Management/MusicManager.cs
+++ Title WIP/Assets/Code/Standard/Management/MusicManager.cs
@@ -9,7 +9,9 @@ public sealed class MusicManager : Manager<MusicManager>
 	private int currentActiveTrack = 0;
 	private bool trackSwitchBuffered = false;
 
-	private float waitTime { get => 60f / bootstrapper.debugBPMs[currentActiveTrack] * 16f - (60f / bootstrapper.debugBPMs[currentActiveTrack] * 16f) * .0075f; }
+	private float trackSwitchSmoothingVal = .15f;
+	private float trackSwitchTime { get => trackSwitchSmoothingVal * (60f / bootstrapper.debugBPMs[currentActiveTrack] * 4f); }
+	private float waitTime { get => 60f / bootstrapper.debugBPMs[currentActiveTrack] * 4f; }
 
 	internal void RegisterBootstrapper(MusicManagerBootstrapper bootstrapper)
 	{
@@ -19,20 +21,33 @@ public sealed class MusicManager : Manager<MusicManager>
 
 	private IEnumerator TrackHandler()
 	{
-		Debug.Log(waitTime);
-		var waiter = new WaitForSeconds(waitTime);
+		var waiter = new WaitForSeconds(waitTime - trackSwitchTime);
+		var trackSwitchWaiter = new WaitForSeconds(trackSwitchTime);
 		bootstrapper.audioSources[currentActiveTrack].Play();
 
 		while (true)
 		{
-			if (!trackSwitchBuffered) yield return waiter;
-			else
+			yield return waiter;
+			if (trackSwitchBuffered)
 			{
 				bootstrapper.audioSources[currentActiveTrack].Stop();
-				bootstrapper.audioSources[++currentActiveTrack].Play();
-				waiter = new WaitForSeconds(waitTime);
+				bootstrapper.audioSources[currentActiveTrack == bootstrapper.debugBPMs.Length ? 0 : currentActiveTrack + 1].Play();
+
 				trackSwitchBuffered = false;
+				waiter = new WaitForSeconds(waitTime - trackSwitchTime);
+				trackSwitchWaiter = new WaitForSeconds(trackSwitchTime);
+				continue;
 			}
+			yield return trackSwitchWaiter;
+
+			//if (!trackSwitchBuffered) yield return waiter;
+			//else
+			//{
+			//	bootstrapper.audioSources[currentActiveTrack].Stop();
+			//	bootstrapper.audioSources[++currentActiveTrack].Play();
+			//	waiter = new WaitForSeconds(waitTime);
+			//	trackSwitchBuffered = false;
+			//}
 		}
 	}
 
