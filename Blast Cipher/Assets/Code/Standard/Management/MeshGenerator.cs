427f12a72e6161d5977a13733456ef96c19b08b1
YDenker <33430355+YDenker@users.noreply.github.com>
2019-07-24 17:40:46 +0200

    fixed Normals on Characters

--- Blast Cipher/Assets/Code/Standard/Management/MeshGenerator.cs
+++ Blast Cipher/Assets/Code/Standard/Management/MeshGenerator.cs
@@ -8,78 +8,50 @@ public class MeshGenerator
     public static Mesh GenerateMeshFromScriptableObject(CScriptableCharacter character)
     {
         Mesh mesh = new Mesh();
-        Vector3[] vertices;
-        int[] triangles;
         CCubeMesh[] cubesMesh = new CCubeMesh[character.CubePositions.Length];
         for (int amount = 0; amount < cubesMesh.Length; amount++)
         {
             cubesMesh[amount] = new CCubeMesh(character.CubePositions[amount]);
         }
+
+        List<BasicMesh> cubesMeshData = new List<BasicMesh>();
+        foreach (var cubeMesh in cubesMesh)
+        {
+            bool[] neighbours = HasNeighbours(cubeMesh.Position,character);
+            cubesMeshData.Add(cubeMesh.GetMeshData(!neighbours[0], !neighbours[1], !neighbours[2], !neighbours[3], !neighbours[4], !neighbours[5]));
+        }
+        BasicMesh complete = new BasicMesh();
+        foreach(var basicMesh in cubesMeshData)
+        {
+            CombineBasicMeshes(ref complete, basicMesh,character);
+        }
+
         if (cubesMesh != null)
         {
-            vertices = ExtractVerticesFromCubicMesh(cubesMesh, character.CharacterScaling, character.Offset);
-            triangles = ExtractTrianglesFromCubicMesh(cubesMesh, character, vertices);
             mesh.Clear();
-            mesh.vertices = vertices;
-            mesh.triangles = triangles;
-            mesh = RecalculateNormals(mesh);
+            mesh.vertices = complete.Vertices.ToArray();
+            mesh.triangles = complete.Triangles.ToArray();
+            mesh.RecalculateNormals();
         }
         return mesh;
     }
 
-    private static Vector3[] ExtractVerticesFromCubicMesh(CCubeMesh[] cubesMesh, int scaling, Vector3 offset)
+    private static void CombineBasicMeshes(ref BasicMesh first, BasicMesh second, CScriptableCharacter character)
     {
-        List<Vector3> verts = new List<Vector3>();
-        for (int amount = 0; amount < cubesMesh.Length; amount++)
+        int triOffset = first.Vertices.Count;
+        foreach (var vert in second.Vertices)
         {
-            for (int pos = 0; pos < 8; pos++)
-            {
-                Vector3 vec = ScaleVector3((cubesMesh[amount].Vertices[pos]-offset),scaling);
-                if (!verts.Contains(vec))
-                    verts.Add(vec);
-            }
+            first.Vertices.Add((vert - character.Offset) / character.CharacterScaling);
         }
-        return verts.ToArray();
+        foreach (var tri in second.Triangles)
+        {
+            first.Triangles.Add(tri + triOffset);
+        }
     }
 
-    private static int[] ExtractTrianglesFromCubicMesh(CCubeMesh[] cubesMesh, CScriptableCharacter character, Vector3[] vertices)
+    private static bool[] HasNeighbours(Vector3Int position, CScriptableCharacter character)
     {
-        List<int> tris = new List<int>();
-        for (int amount = 0; amount < cubesMesh.Length; amount++)
-        {
-            for (int direction = 0, position = 0; direction < 6; direction++)
-            {
-                for (int pos = 0; pos < 6; pos++, position++)
-                {
-                    if (!HasNeighbor(direction, character.CubePositions[amount],character))
-                        tris.Add(PositionOfItemInArray(vertices, ScaleVector3(cubesMesh[amount].Vertices[cubesMesh[amount].Triangles[position]] - character.Offset, character.CharacterScaling)));
-                }
-            }
-        }
-        return tris.ToArray();
-    }
-
-    private static bool HasNeighbor(int side, Vector3Int position, CScriptableCharacter character) // 0Front, 1Back, 2Left, 3Right, 4Up, 5Down
-    {
-        switch (side)
-        {
-            case 0:
-                return HasCubeAt(position + new Vector3Int(0, 0, 1),character);
-            case 1:
-                return HasCubeAt(position + new Vector3Int(0, 0, -1), character);
-            case 2:
-                return HasCubeAt(position + new Vector3Int(1, 0, 0), character);
-            case 3:
-                return HasCubeAt(position + new Vector3Int(-1, 0, 0), character);
-            case 4:
-                return HasCubeAt(position + new Vector3Int(0, 1, 0), character);
-            case 5:
-                return HasCubeAt(position + new Vector3Int(0, -1, 0), character);
-            default:
-                Debug.Log("Error");
-                break;
-        }
-        return false;
+        return new bool[] { HasCubeAt(position + new Vector3Int(0, 0, 1), character), HasCubeAt(position + new Vector3Int(0, 0, -1), character), HasCubeAt(position + new Vector3Int(1, 0, 0), character), HasCubeAt(position + new Vector3Int(-1, 0, 0), character), HasCubeAt(position + new Vector3Int(0, 1, 0), character), HasCubeAt(position + new Vector3Int(0, -1, 0), character) };
     }
 
     private static bool HasCubeAt(Vector3Int pos, CScriptableCharacter character)
@@ -90,28 +62,5 @@ public class MeshGenerator
                 return true;
         }
         return false;
-    }
-
-    private static Mesh RecalculateNormals(Mesh mesh)
-    {
-        mesh.RecalculateNormals();
-        // I don't fucking know how...
-        return mesh;
-    }
-
-    private static int PositionOfItemInArray<T>(T[] array, T item)
-    {
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (item.Equals(array[i]))
-                return i;
-        }
-        return -1;
-    }
-
-    private static Vector3 ScaleVector3(Vector3 vector, int scaling)
-    {
-        Vector3 outVector = new Vector3(vector.x * (1f / scaling), vector.y * (1f / scaling), vector.z * (1f / scaling));
-        return outVector;
     }
 }
